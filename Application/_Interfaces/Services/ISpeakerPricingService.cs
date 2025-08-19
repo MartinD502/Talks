3c8f38b2f3a10889376487f97e39d860e9505fe4
martin.duncan2000s <martin.duncan2000s@gmail.com>
2025-08-19 13:46:32 +0100

    Amended due to a couple of references not needed.

--- Application/_Interfaces/Services/ISpeakerPricingService.cs
+++ Application/_Interfaces/Services/ISpeakerPricingService.cs
@@ -1,5 +1,3 @@
-using Application.Dtos;
-
 namespace Application._Interfaces.Services
 {
     public interface ISpeakerPricingService
--- Talks.Tests/Unit/Services/SessionApprovalServiceTests.cs
+++ Talks.Tests/Unit/Services/SessionApprovalServiceTests.cs
@@ -1,10 +1,5 @@
 ﻿using Application.Dtos;
 using Application.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Talks.Tests.Unit.Services
